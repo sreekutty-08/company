e486ca275623d66b3284a53241ee0319375f8b2e
sreekutty-08 <sreekuttysree584@gmail.com>
2025-02-19 15:55:15 +0530

    third commit

--- src/servers.jsx
+++ src/servers.jsx
@@ -1,1 +1,11 @@
-export const SERVER = "https://telcom-backend-tigo.onrender.com/"
+export const SERVER = "https://telcom-backend-tigo.onrender.com/api/";
+
+fetch(`${SERVER}company/`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => console.log("Data:", data))
+  .catch(error => console.error("Error fetching data:", error));
