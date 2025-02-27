824e49f9bc56bf3dd5a5ab61dc97684e37de4881
shanu <ashanu2004@gmail.com>
2025-02-27 23:35:11 +0530

    change style in main page

--- src/servers.jsx
+++ src/servers.jsx
@@ -1,2 +1,2 @@
-export const SERVER = "https://telecom.cloudqlobe.com/"  //https://telecom.cloudqlobe.com/ //http://localhost:5000/
+export const SERVER = "http://localhost:5000/"  //https://telecom.cloudqlobe.com/ //http://localhost:5000/
 // export const SERVER = "http://127.0.0.1:8000/"
